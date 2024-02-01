d1eb04428a9fc00c4785a28c4f3fbc9a15d46177
Onur Temizkan <onur@narval.co.uk>
2024-02-01 12:15:22 +0000

    Fix linter

--- packages/remix/src/index.server.ts
+++ packages/remix/src/index.server.ts
@@ -111,6 +111,7 @@ export * from '@sentry/node';
 
 export {
   captureRemixServerException,
+  // eslint-disable-next-line deprecation/deprecation
   wrapRemixHandleError,
   sentryHandleError,
   wrapHandleErrorWithSentry,
